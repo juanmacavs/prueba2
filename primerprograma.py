c30edce1fa0db21c0023af861b832a7fcfcf6767
JuanMa <ziro_cool22@protonmail.com>
2021-03-09 16:18:02 -0300

    Se agregaron mayor cantidad de numeros

--- primerprograma.py
+++ primerprograma.py
@@ -1,6 +1,8 @@
 n1 = int(input('ingrese primer numero: '))
 n2 = int(input('ingrese segundo numero: '))
 n3 = int(input('ingrese tercer numero: '))
+n4 = int(input('ingrese cuarto numero: '))
+n5 = int(input('ingrese quito numero: '))
 menor = min(n1, n2, n3)
 mayor = max (n1, n2, n3)
 valormedio = (n1 + n2 + n3) - menor - mayor
